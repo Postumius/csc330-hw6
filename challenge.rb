fd65391a4a443f7a073993e812deadb4ce8b2533
Noah Ma <noahfierz@gmail.com>
2020-03-09 15:08:41 -0700

    auto works a little

--- challenge.rb
+++ challenge.rb
@@ -3,9 +3,14 @@ require_relative './hw6provided'
 class MyPieceChallenge < Piece
   All_Pieces = [[[[0, 0]]]]
 
+   #returns number of squares in block
+  def size
+    @all_rotations[0].size
+  end
+
    # class method to choose the next piece
   def self.next_piece (board)
-    Piece.new(All_Pieces.sample, board)
+    MyPieceChallenge.new(All_Pieces.sample, board)
   end
 
   def move_to_best
@@ -15,35 +20,40 @@ class MyPieceChallenge < Piece
 
   private
   def pick_spot (spots)
+    max = [0, 0]
     spots.each do |point|
-      if point[1] > -1
-      then return point
+      if point[1] > max[1]
+      then max = point     
       end
     end
+    max
   end
 
   def potential_spots
-    (0..@board.num_collumns).each do |x|
+    (0..@board.num_columns-1).map do |x|
       [x, lowest_fit(@all_rotations[0], x)]
     end
   end
 
   def fits? (shape, x, y)
     shape.each do |point|
-      if !board.empty_at(point)
-      then return false
+      location = [point[0]+x, point[1]+y]
+      if !@board.empty_at(location)
+      then
+        puts(point)
+        return false
       end
     end
     return true
   end
 
   def lowest_fit(shape, x)
-    (num_rows..0).each do |y|
+    (@board.num_rows-1).downto(0).each do |y|
       if fits?(shape, x, y)
       then return y
       end
     end
-    -1
+    raise("no fit in column")
   end
   
 end
@@ -54,10 +64,33 @@ class MyBoardChallenge < Board
     @current_block = MyPieceChallenge.next_piece(self)
   end
 
+  def auto
+    if !game_over? and @game.is_running?
+      @current_block.move_to_best
+    end
+    draw
+  end
+
   # gets the next piece
   def next_piece
-    @current_block = Piece.next_piece(self)
+    @current_block = MyPieceChallenge.next_piece(self)
     @current_pos = nil
+  end
+
+ 
+
+  # gets the information from the current piece about where it is and uses this
+  # to store the piece on the board itself.  Then calls remove_filled.
+  def store_current
+    locations = @current_block.current_rotation
+    displacement = @current_block.position
+    (0...@current_block.size).each{|index| 
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
   end
   
 end
@@ -69,5 +102,11 @@ class MyTetrisChallenge < Tetris
     @canvas.place(@board.block_size * @board.num_rows + 3,
                   @board.block_size * @board.num_columns + 6, 24, 80)
     @board.draw
-  end
+   end
+
+   def key_bindings
+     super
+     @root.bind('a' , proc {@board.auto})
+   end  
+   
 end
--- hw6runner.rb
+++ hw6runner.rb
@@ -2,6 +2,7 @@
 
 require_relative './hw6provided'
 require_relative './hw6assignment'
+require_relative './challenge.rb'
 
 def runTetris
   Tetris.new 
@@ -13,15 +14,22 @@ def runMyTetris
   mainLoop
 end
 
+def runMyTetrisChallenge
+  MyTetrisChallenge.new
+  mainLoop
+end
+
 if ARGV.count == 0
   runMyTetris
 elsif ARGV.count != 1
-  puts "usage: hw6runner.rb [enhanced | original]"
+  puts "usage: hw6runner.rb [enhanced | original | challenge]"
 elsif ARGV[0] == "enhanced"
   runMyTetris
 elsif ARGV[0] == "original"
   runTetris
+elsif ARGV[0] == "challenge"
+  runMyTetrisChallenge
 else
-  puts "usage: hw6runner.rb [enhanced | original]"
+  puts "usage: hw6runner.rb [enhanced | original | challenge]"
 end
 
